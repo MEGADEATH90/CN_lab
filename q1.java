6c3a61792e8f790adcf9fc39d8d79d65aa7da71b
MEGADEATH90 <91825244+MEGADEATH90@users.noreply.github.com>
2023-09-28 13:06:54 +0530

    Add files via upload

---
+++ q1.java
@@ -0,0 +1,14 @@
+package exp2;
+import java.io.*;
+import java.util.Scanner;
+public class q1 {
+
+	public static void main(String[] args) throws IOException
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a char : ");
+		char a = sc.next().charAt(0);
+		System.out.println(a);
+	}
+
+}
